0ba9cb351aa0ea5e27ec630ffded69ceda7ad2e9
Shahin Amon <amons@bzz.ch>
2024-07-11 14:47:58 +0200

    added booking controllers

---
+++ src/main/java/ch/zli/m223/controller/booking/BookingControllerMember.java
@@ -0,0 +1,54 @@
+package ch.zli.m223.controller.booking;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import ch.zli.m223.model.Booking;
+import ch.zli.m223.model.impl.BookingImpl;
+import ch.zli.m223.service.booking.BookingService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+
+@RestController
+@RequestMapping("/api/v1/member/bookings")
+public class BookingControllerMember {
+
+    @Autowired
+    private BookingService bookingService;
+
+    @GetMapping
+    List<Booking> getAllBookings(){
+        return bookingService.getAllBookings();
+    }
+
+    @GetMapping("/{bookingId}")
+    Booking getBookingByBookingId(@PathVariable("bookingId") Long bookingId){
+        return bookingService.getBookingByBookingId(bookingId);
+    }
+
+    @GetMapping("/{userId}")
+    List<BookingImpl> getBookingByUserId(@PathVariable("userId") Long userId){
+        return bookingService.getBookingByUserId(userId);
+    }
+
+    @PostMapping
+    public BookingImpl createBooking(@RequestBody Long userid, String bookingDate, Booking.Duration duration){
+        return bookingService.createBooking(userid, bookingDate, duration);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBooking(@PathVariable("id") Long id){
+        bookingService.deleteBooking(id);
+    }
+    
+}
