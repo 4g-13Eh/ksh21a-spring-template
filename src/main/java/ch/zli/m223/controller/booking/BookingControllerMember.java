6842b1c220961478c6703a2068b6a68ace36e95e
Shahin Amon <amons@bzz.ch>
2024-07-11 16:07:14 +0200

    added booking dto

--- src/main/java/ch/zli/m223/controller/booking/BookingControllerMember.java
+++ src/main/java/ch/zli/m223/controller/booking/BookingControllerMember.java
@@ -1,13 +1,17 @@
 package ch.zli.m223.controller.booking;
 
+import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import ch.zli.m223.controller.booking.dto.BookingDto;
+import ch.zli.m223.model.AppUser;
 import ch.zli.m223.model.Booking;
 import ch.zli.m223.model.impl.BookingImpl;
 import ch.zli.m223.service.booking.BookingService;
+import ch.zli.m223.service.user.UserService;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,6 +30,9 @@ public class BookingControllerMember {
     @Autowired
     private BookingService bookingService;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
     List<Booking> getAllBookings(){
         return bookingService.getAllBookings();
@@ -36,14 +43,15 @@ public class BookingControllerMember {
         return bookingService.getBookingByBookingId(bookingId);
     }
 
-    @GetMapping("/{userId}")
+    @GetMapping("/user/{userId}")
     List<BookingImpl> getBookingByUserId(@PathVariable("userId") Long userId){
         return bookingService.getBookingByUserId(userId);
     }
 
     @PostMapping
-    public BookingImpl createBooking(@RequestBody Long userid, String bookingDate, Booking.Duration duration){
-        return bookingService.createBooking(userid, bookingDate, duration);
+    public BookingImpl createBooking(@RequestBody BookingDto bookingDto, Principal principal){
+        AppUser user = userService.getUserByName(principal.getName());
+        return bookingService.createBooking(user.getId(), bookingDto.getBookingDate(), bookingDto.getDuration());
     }
 
     @DeleteMapping("/{id}")
