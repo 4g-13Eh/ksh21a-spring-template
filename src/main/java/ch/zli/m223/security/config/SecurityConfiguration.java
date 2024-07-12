82bcb58ba9355dc6f9b30177f7401e8c3be4100d
Shahin Amon <amons@bzz.ch>
2024-07-12 12:55:49 +0200

    added openapi doc

--- src/main/java/ch/zli/m223/security/config/SecurityConfiguration.java
+++ src/main/java/ch/zli/m223/security/config/SecurityConfiguration.java
@@ -37,6 +37,8 @@ public class SecurityConfiguration {
                         .requestMatchers("/error").permitAll()
 
                         // Application security
+                        .requestMatchers("/swagger-ui/**").permitAll() 
+                        .requestMatchers("/v3/api-docs/**").permitAll() 
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/api/v1/admin/**").hasAuthority(UserRoles.admin)
                         .requestMatchers("/api/v1/member/**").hasAuthority(UserRoles.member)
